55bd7f0108a262a81ba0e5c5555c62d2e0da0120
Marcus Moreno <legendary.1198@gmail.com>
2020-07-20 21:53:48 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,66 +1,63 @@
-class Artist 
-  
+  class Artist
+    
   @@all = []
   
   attr_accessor :name, :songs
-  
-  def intitialize(name)
+
+  def initialize(name)
     
     @name = name
     
-    save 
+    save
     
   end
   
   
-  def self.all 
+  def self.all
     
-    @@all
+    @@all  
     
   end
   
   
   def add_song(song)
     
-    song.artist = self 
+    song.artist = self
     
   end
   
-  
-  def song 
+
+  def songs
     
     Song.all.select {|song| song.artist == self}
     
   end
   
-  
-def self.find_or_create_by_name(name)
-  
-  self.find(name) ? self.find(name) : self.new(name)
-  
-end
+
+  def self.find_or_create_by_name(name)
+    
+    self.find(name) ? self.find(name) : self.new(name)
+    
+  end
 
 
-def self.find(name)
+  def self.find(name)
+    
+    self.all.find {|artist| artist.name == name }
+    
+  end
   
-  self.all.find  {|artist| artist.name == name}
-  
-end
 
+  def save
+    
+    @@all << self
+    
+  end
+  
 
-def save
-  
-  @@all << self
-  
-end
-
-
-def print_songs 
-  
-  songs.each {|song| puts song.new}
-  
+  def print_songs
+    
+    songs.each {|song| puts song.name}
+    
   end
 end
-
-  
-  
