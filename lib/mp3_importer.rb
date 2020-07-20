96942dead13f1b3ecda2f664082d79fceedb73dd
Jackson Reynolds <jwreynold@gmail.com>
2020-07-20 19:31:32 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,5 +1,6 @@
-
-
+require 'artist.rb'
+require 'song.rb'
+require 'pry'
 
 class MP3Importer
 
@@ -8,5 +9,17 @@ class MP3Importer
   def initialize(file_path)
     @path = file_path
   end #initialize
+
+  def files
+    files = Dir[self.path+'/*.mp3'].collect do |file|
+      file.slice!(self.path+'/')
+      file
+    end #do
+    # binding.pry
+  end #files
+
+  def import
+    files.each {|file| Song.new_by_filename(file)}
+  end #import
 
 end #MP3Importer
